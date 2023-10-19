12b36395939c19bc03008bf4234cf9bf657dd85e
zack3rsgroup <127765124+zack3rsgroup@users.noreply.github.com>
2023-10-19 22:38:16 +0300

    Add files via upload

---
+++ Orcus.Patcher/Plugin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Mono.Cecil;
+using Orcus.Administration.Plugins;
+using Orcus.Administration.Plugins.BuildPlugin;
+
+namespace OrcusPatcher
+{
+	public class Plugin : BuildPluginBase
+	{
+		public override void Prepare(IBuilderArguments builderArguments)
+		{
+			builderArguments.SubscribeBuilderEvent(new ModifyAssemblyBuilderEvent(new ModifyAssemblyBuilderEvent.ModifyAssemblyDelegate(this.ModifyAssemblyDelegate)));
+		}
+
+		private void ModifyAssemblyDelegate(IBuilderInformation builderInformation, AssemblyDefinition assemblyDefinition)
+		{
+			builderInformation.BuildLogger.Status("Поиск Кода для Удаления!");
+			bool flag = false;
+			foreach (TypeDefinition typeDefinition in assemblyDefinition.Modules[0].Types)
+			{
+				if (typeDefinition.FullName == "Orcus.OrcusApplicationContext")
+				{
+					foreach (MethodDefinition methodDefinition in typeDefinition.Methods)
+					{
+						if (methodDefinition.Name == ".ctor")
+						{
+							methodDefinition.Body.Instructions.RemoveAt(methodDefinition.Body.Instructions.Count - 2);
+							methodDefinition.Body.Instructions.RemoveAt(methodDefinition.Body.Instructions.Count - 2);
+							flag = true;
+							break;
+						}
+					}
+				}
+			}
+			if (!flag)
+			{
+				throw new PluginException("Patcher: Не найден лишний код для удаления!");
+			}
+			builderInformation.BuildLogger.Status("IL Код успешно удален!");
+		}
+	}
+}
